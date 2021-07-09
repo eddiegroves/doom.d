a85178ae905c129d6e305086f5933e9dfc8e709b
Eddie Groves <eddiegroves@gmail.com>
2021-07-10 08:30:12 +1000

    Fresh doom config

---
+++ config.el
@@ -0,0 +1,6 @@
+(setq user-full-name "Eddie Groves"
+      user-mail-address "eddiegroves@gmail.com")
+
+(setq doom-font (font-spec :family "Hack Nerd Font" :size 18 :weight 'semi-light))
+
+(setq display-line-numbers-type nil)
